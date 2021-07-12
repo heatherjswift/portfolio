0b74467a42aad1bcd037b6381bfa9ced7e7173c5
Heather Swift <78330855+heatherjswift@users.noreply.github.com>
2021-07-11 23:06:45 -0400

    add content to resume page

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -2,6 +2,24 @@ function Resume() {
   return (
     <section>
       <h1>Resume</h1>
+
+      <a href="">Download my resume here! (in the future...)</a>
+
+      <h3>Proficient with:</h3>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>GitHub</li>
+        <li>JavaScript</li>
+        <li>Third-Party APIs</li>
+        <li>Server-Side APIs</li>
+        <li>Node.js</li>
+        <li>Express.js</li>
+        <li>SQL</li>
+        <li>NoSQL</li>
+        <li>Progressive Web Applications (PWA)</li>
+        <li>React</li>
+      </ul>
     </section>
   )
 }
